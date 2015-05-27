068a6157ff7c4796054a9cf0f5b07bcfc70d6e1f
James White <dev.jameswhite@gmail.com>
2015-05-27 10:25:30 +0100

    Support for months in times also, oops.

--- public/js/filters.js
+++ public/js/filters.js
@@ -16,15 +16,20 @@ angular.module('tsadminFilters', []).filter('prettyUptime', function() {
       var hours = Math.floor(Math.floor(time % 86400) / 3600);
       return days + 'd ' + hours + 'h';
     // Weeks
-    } else if (time > 604800 && time < 31449600) {
+    } else if (time > 604800 && time < 2419200) {
       var weeks = Math.floor(time / 604800);
       var days = Math.floor(Math.floor(time % 604800) / 86400);
       return weeks + 'w ' + days + 'd';
+    // Months
+    } else if (time > 2419200 && time < 31449600) {
+      var months = Math.floor(time / 2419200);
+      var weeks = Math.floor(Math.floor(time % 2419200) / 604800);
+      return months + 'm ' + weeks + 'w';
     // Years!
     } else if (time > 31449600) {
       var years = Math.floor(time / 31449600);
-      var weeks = Math.floor(Math.floor(time % 31449600) / 604800);
-      return weeks + 'y ' + days + 'd';
+      var months = Math.floor(Math.floor(time % 31449600) / 2419200);
+      return years + 'y ' + months + 'm';
     // Seconds
     } else {
       return time + 's';
